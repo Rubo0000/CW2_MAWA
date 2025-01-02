1a987acce4b5c022c43a13ee081d697bdb0f6aa3
Rubén Arranz Martínez <ruben_arranz_@hotmail.com>
2025-01-02 19:54:48 +0000

    Restarting backend

--- back/main.py
+++ back/main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-from app.database import engine, Base
-from app.routes import users
-
-# Create the database tables
-Base.metadata.create_all(bind=engine)
+import random
 
 app = FastAPI()
 
-# Include user routes
-app.include_router(users.router, prefix="/api", tags=["Users"])
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to the Shangri-La backend!"}
+@app.get('/')
+async def root():
+    return {'message': 'Welcome to the Shangri-La backend!'}
--- back/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class UserCreate(BaseModel):
-    email: str
-    name: str
-    password: str
-    bio_id: str
-    is_committee: Optional[bool] = False
